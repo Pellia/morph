f58567b361469491ce0d1e22928589f9c87a2eba
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-05-10 16:07:39 +0200

    init: frontend

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="flex justify-center mt-4">
+            <h1 className="font-bold text-4xl">Project Morphling</h1>
+        </div>
+    );
+};
+
+export default App;
